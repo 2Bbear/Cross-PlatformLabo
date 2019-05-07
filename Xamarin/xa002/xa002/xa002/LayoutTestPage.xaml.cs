5cbf98ddbd806ed183b2c22f2703e3250de863da
Jangjungho <izex.jangjungho@gmail.com>
2019-05-07 10:44:19 +0900

    레이아웃테스트 페이지 추가

---
+++ Xamarin/xa002/xa002/xa002/LayoutTestPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace xa002
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LayoutTestPage : ContentPage
+	{
+		public LayoutTestPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
