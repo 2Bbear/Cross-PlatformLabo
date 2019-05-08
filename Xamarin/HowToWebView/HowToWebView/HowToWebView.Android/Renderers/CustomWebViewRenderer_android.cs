380b5042c0d4a63c3cf83803ec9a73c0fd6fa3d2
Jangjungho <izex.jangjungho@gmail.com>
2019-05-08 13:33:29 +0900

    페이지 화면 출력이 이상하게 되는 오류 수정

--- Xamarin/HowToWebView/HowToWebView/HowToWebView.Android/Renderers/CustomWebViewRenderer_android.cs
+++ Xamarin/HowToWebView/HowToWebView/HowToWebView.Android/Renderers/CustomWebViewRenderer_android.cs
@@ -16,7 +16,6 @@ namespace HowToWebView.Droid.Renderers
     {
         Context _context;
         public static CustomWebViewRenderer_android getInstance { get; set; }
-        private  Stack<string> urlHistory;
         public Android.Webkit.WebView wv;
         //public class UrlChanger_android : IWebView
         //{
@@ -38,7 +37,6 @@ namespace HowToWebView.Droid.Renderers
         {
             _context = context;
             getInstance = this;
-            urlHistory = new Stack<string>();
         }
         /// <summary>
         /// 관련된 프로퍼티가 무언가 변경되었을때 반응하는 메소드
@@ -53,7 +51,6 @@ namespace HowToWebView.Droid.Renderers
             {
                 Xamarin.Forms.WebView wv = sender as Xamarin.Forms.WebView;
                 string targetUrl = (wv.Source as UrlWebViewSource).Url;
-                urlHistory.Push(targetUrl);
                 Control.LoadUrl(targetUrl);
             }
         }
@@ -67,29 +64,42 @@ namespace HowToWebView.Droid.Renderers
             if (Control == null)
             {
                 wv = new Android.Webkit.WebView(_context);
-                wv.Settings.JavaScriptEnabled = true;
-                wv.Settings.CacheMode = CacheModes.CacheElseNetwork;
-                wv.Settings.SetAppCacheEnabled(false);
-                wv.Settings.DomStorageEnabled = true;
-                wv.Settings.SetSupportZoom(true);
+
+                Android.Webkit.WebView.SetWebContentsDebuggingEnabled(true);
+               
+                wv.Settings.JavaScriptEnabled = true; // javacript로 이루어진 기능을 사용하기 위함
+                wv.Settings.JavaScriptCanOpenWindowsAutomatically = true;//javacript로 팝업창을 띄울 경우가 있는데, 이걸 사용해야 가능함
+                wv.Settings.LoadsImagesAutomatically = true; //WebView가 app에 등록되어 있는 이미지 리소스를 자동으로 로드하도록 설정하는 속성
+                //wv.Settings.UseWideViewPort = true;//WebView가 wide viewport를 사용하도록 설정하는 속성, 따라서 html 컨텐츠가 WebView에 맞춰서 나타남
+                wv.Settings.CacheMode = CacheModes.CacheElseNetwork; // 웹뷰의 캐시모드 설정
+                wv.Settings.SetAppCacheEnabled(false); // app 내부 캐시 사용 여부 설정
+                wv.Settings.DomStorageEnabled = true;// 로컬 스토리지 사용 여부 설정, ex 하루동안 보지 않기 기능에 필요함
+                wv.Settings.AllowFileAccess = true;// WebView 내에서 파일 액세스 활성화 여부
+                wv.SetWebChromeClient(new WebChromeClient());
+                //wv.Settings.UserAgentString = "app"; // 웹에서 해당 속성을 통해 앱에서 띄운 웹뷰로 인지 할 수 있도록 하기 위함.app으로 할경우 웹페이지가 pc 화면으로 출력됨
+                wv.Settings.SetSupportZoom(true);//확대 축소 기능을 사용할 수 있도록 설정하는 속성
+                wv.Settings.BuiltInZoomControls = true;
                 wv.Settings.AllowUniversalAccessFromFileURLs = true;
+                //wv.Settings.BlockNetworkImage = false;
+                //wv.VerticalScrollBarEnabled = true; //세로 스크롤을 사용 할 수 있는지 여부
                 wv.SetWebViewClient(new MyWebViewClient());
-                
+
+#pragma warning disable CS0618 // 형식 또는 멤버는 사용되지 않습니다.
+                wv.LayoutParameters=new Android.Widget.AbsoluteLayout.LayoutParams(LayoutParams.MatchParent, LayoutParams.MatchParent, 0, 0);//특수한 케이스로 페이지의 자바스크립트를 올바르게 실행하지 못해 페이지 위치를 잘못 지정할 경우 이 문장을 사용해야한다.
+#pragma warning restore CS0618 // 형식 또는 멤버는 사용되지 않습니다.
                 SetNativeControl(wv);
             }
             if (e.OldElement != null)
             {
+                //Web에서 실행되는 JavaScript 메소드에 반응하기 위한 구문.
                 //Control.RemoveJavascriptInterface("jsBridge");
                 //var hybridWebView = e.OldElement as HybridWebView;
                 //hybridWebView.Cleanup();
             }
             if (e.NewElement != null)
             {
-              
-                Control.LoadUrl("http:naver.com");
-                urlHistory.Push("http:naver.com");
-
-
+                //"http:naver.com"
+                wv.LoadUrl("http:naver.com");
             }
         } 
     }
@@ -103,5 +113,24 @@ namespace HowToWebView.Droid.Renderers
         {
             base.OnPageStarted(view, url, favicon);
         }
+
+        public override void OnPageFinished(Android.Webkit.WebView view, string url)
+        {
+            base.OnPageFinished(view, url);
+        }
+
+
+        //public override bool ShouldOverrideUrlLoading(Android.Webkit.WebView view, string url)
+        //{
+        //    view.LoadUrl(url);
+        //    return false;
+        //}
+        //For API level 24 and later
+        //public override bool ShouldOverrideUrlLoading(Android.Webkit.WebView view, IWebResourceRequest request)
+        //{
+        //    return base.ShouldOverrideUrlLoading(view, request);
+        //}
+
+
     }
 }
--- Xamarin/HowToWebView/HowToWebView/HowToWebView.Android/obj/Debug/81/android/src/md594a19b7e6d2928db9de703e2d139876f/MyWebViewClient.java
+++ Xamarin/HowToWebView/HowToWebView/HowToWebView.Android/obj/Debug/81/android/src/md594a19b7e6d2928db9de703e2d139876f/MyWebViewClient.java
@@ -11,6 +11,7 @@ public class MyWebViewClient
 	static {
 		__md_methods = 
 			"n_onPageStarted:(Landroid/webkit/WebView;Ljava/lang/String;Landroid/graphics/Bitmap;)V:GetOnPageStarted_Landroid_webkit_WebView_Ljava_lang_String_Landroid_graphics_Bitmap_Handler\n" +
+			"n_onPageFinished:(Landroid/webkit/WebView;Ljava/lang/String;)V:GetOnPageFinished_Landroid_webkit_WebView_Ljava_lang_String_Handler\n" +
 			"";
 		mono.android.Runtime.register ("HowToWebView.Droid.Renderers.MyWebViewClient, HowToWebView.Android", MyWebViewClient.class, __md_methods);
 	}
@@ -30,6 +31,14 @@ public class MyWebViewClient
 	}
 
 	private native void n_onPageStarted (android.webkit.WebView p0, java.lang.String p1, android.graphics.Bitmap p2);
+
+
+	public void onPageFinished (android.webkit.WebView p0, java.lang.String p1)
+	{
+		n_onPageFinished (p0, p1);
+	}
+
+	private native void n_onPageFinished (android.webkit.WebView p0, java.lang.String p1);
 
 	private java.util.ArrayList refList;
 	public void monodroidAddReference (java.lang.Object obj)
